7e916f6b99e1290d679dc8968af2bdfc913fa772
Mike Brown <brownbk@merrimack.edu>
2026-03-27 11:49:10 -0400

    setup and database connection management

---
+++ src/DatabaseConnectionManager.java
@@ -0,0 +1,71 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class DatabaseConnectionManager {
+
+    private static Connection mealPlanningConnection;
+    private static Connection arcadeGamesConnection;
+
+    private static final String HOST = "localhost";
+    private static final String PORT = "3306";
+    private static final String USER = "root";
+    private static final String PASSWORD = "xyz";
+
+    /**
+     * takes a database name and returns a connection to that database
+     * @param databaseName name of the database we're trying to connect to
+     * @return connection to that database
+     * @throws SQLException if the database doesn't exist or can't be connected to
+     */
+    private static Connection getConnection(String databaseName) throws SQLException {
+        String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + databaseName;
+        return DriverManager.getConnection(url, USER, PASSWORD);
+    }
+
+    /**
+     * gets the connection for the MealPlanning database
+     * @return a connection to the MealPlanning database
+     * @throws SQLException if the database doesn't exist or can't be connected to
+     */
+    public static Connection getMealPlanningConnection() throws SQLException {
+        if (mealPlanningConnection == null || mealPlanningConnection.isClosed()) {
+            mealPlanningConnection = getConnection("MealPlanning");
+        }
+        return mealPlanningConnection;
+    }
+
+    /**
+     * gets the connection to the ArcadeGames databse
+     * @return a conection to the ArcadeGames database
+     * @throws SQLException if the database doesn't exist or can't be connected to
+     */
+    public static Connection getArcadeGamesConnection() throws SQLException {
+        if (arcadeGamesConnection == null || arcadeGamesConnection.isClosed()) {
+            arcadeGamesConnection = getConnection("ArcadeGames");
+        }
+        return arcadeGamesConnection;
+    }
+
+    /**
+     * cleanup, closes the connections
+     */
+    public static void closeConnections() {
+        try {
+            if (mealPlanningConnection != null && !mealPlanningConnection.isClosed()) {
+                mealPlanningConnection.close();
+            }
+            if (arcadeGamesConnection != null && !arcadeGamesConnection.isClosed()) {
+                arcadeGamesConnection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error closing connections: " + e.getMessage());
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    /**
+     * yalalala seclap on discotac
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
